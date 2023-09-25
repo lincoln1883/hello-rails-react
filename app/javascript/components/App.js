b8dcba1911fb5a4bc83dc339fb45721457903a35
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-25 18:19:38 -0300

    Create a app component.

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <main>
+          <h1>Rails Playground</h1>
+        <div className="container"></div>
+    </main>);
+}
+
+export default App;
