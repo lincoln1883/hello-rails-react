94c4f540578de78f845979600802c605dd18d069
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-26 12:15:31 -0300

    Import router provider and store.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.js';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+          <App/>
+      </BrowserRouter>
+    </Provider>
+);
